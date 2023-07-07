c2e2f0aacc62ec68d857721d0d029f454031a183
akopton <kopton.olek@gmail.com>
2023-07-07 14:51:38 +0200

    message

--- src/components/SignInBtn/SignInBtn.tsx
+++ src/components/SignInBtn/SignInBtn.tsx
@@ -2,33 +2,25 @@ import { signIn, signOut, useSession } from "next-auth/react";
 export const SignInBtn = () => {
   const session = useSession();
 
-  const handleSignIn = () => {
-    try {
-      signIn();
-    } catch (e) {
-      console.error(e);
-    }
-  };
+  // const handleSignIn = async () => {
+  //   try {
+  //     await signIn();
+  //   } catch (e) {
+  //     console.error(e);
+  //   }
+  // };
 
-  const handleSignOut = () => {
-    try {
-      signOut();
-    } catch (e) {
-      console.error(e);
-    }
-  };
+  // const handleSignOut = () => {
+  //   try {
+  //     signOut();
+  //   } catch (e) {
+  //     console.error(e);
+  //   }
+  // };
 
   if (session.status === "authenticated") {
-    return (
-      <button className="btn" onClick={handleSignOut}>
-        Sign Out
-      </button>
-    );
+    return <button className="btn">Sign Out</button>;
   }
 
-  return (
-    <button className="btn" onClick={handleSignIn}>
-      Sign In
-    </button>
-  );
+  return <button className="btn">Sign In</button>;
 };
