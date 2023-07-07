b90b829e3219a80d6c91ad96d661874524e881d7
akopton <kopton.olek@gmail.com>
2023-07-07 15:07:27 +0200

    sign in and out error handling

--- src/components/SignInBtn/SignInBtn.tsx
+++ src/components/SignInBtn/SignInBtn.tsx
@@ -3,33 +3,27 @@ export const SignInBtn = () => {
   const session = useSession();
 
   const handleSignIn = async () => {
-    await signIn();
+    await signIn().catch((e) => {
+      throw new Error(e);
+    });
   };
 
   const handleSignOut = async () => {
-    await signOut();
+    await signOut().catch((e) => {
+      throw new Error(e);
+    });
   };
 
   if (session.status === "authenticated") {
     return (
-      <button
-        className="btn"
-        onClick={() => {
-          signIn();
-        }}
-      >
+      <button className="btn" onClick={() => handleSignIn()}>
         Sign Out
       </button>
     );
   }
 
   return (
-    <button
-      className="btn"
-      onClick={() => {
-        signOut();
-      }}
-    >
+    <button className="btn" onClick={() => handleSignOut()}>
       Sign In
     </button>
   );
