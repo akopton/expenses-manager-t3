be1b7001f80220b56f7c008de18e9245e094057d
akopton <kopton.olek@gmail.com>
2023-07-08 22:34:59 +0200

    protected route fix

--- src/components/ProtectedRoute/ProtectedRoute.tsx
+++ src/components/ProtectedRoute/ProtectedRoute.tsx
@@ -1,25 +1,19 @@
 import { useSession } from "next-auth/react";
-import { useRouter } from "next/router";
+import { useRouter as useNavRouter } from "next/navigation";
+import { useRouter as useRouteRouter } from "next/router";
 import type { ReactNode } from "react";
 
 export const ProtectedRoute = ({ children }: { children: ReactNode }) => {
   const session = useSession();
-  const router = useRouter();
+  const router = useNavRouter();
+  const { pathname } = useRouteRouter();
 
-  const pushHome = async () => {
-    await router.push("/");
-  };
-
-  const pushDashboard = async () => {
-    await router.push("/dashboard");
-  };
-
-  if (session.status === "unauthenticated" && router.pathname !== "/") {
-    pushHome();
+  if (session.status === "unauthenticated" && pathname !== "/") {
+    router.push("/");
   }
 
-  if (session.status === "authenticated" && router.pathname === "/") {
-    pushDashboard();
+  if (session.status === "authenticated" && pathname === "/") {
+    router.push("/dashboard");
   }
 
   return children;
