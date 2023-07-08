9ed2fc7e485216a6c486a48faa1c78d0bf878b9a
akopton <kopton.olek@gmail.com>
2023-07-08 22:31:14 +0200

    protected route fix

--- src/components/ProtectedRoute/ProtectedRoute.tsx
+++ src/components/ProtectedRoute/ProtectedRoute.tsx
@@ -6,12 +6,20 @@ export const ProtectedRoute = ({ children }: { children: ReactNode }) => {
   const session = useSession();
   const router = useRouter();
 
+  const pushHome = async () => {
+    await router.push("/");
+  };
+
+  const pushDashboard = async () => {
+    await router.push("/dashboard");
+  };
+
   if (session.status === "unauthenticated" && router.pathname !== "/") {
-    router.push("/");
+    pushHome();
   }
 
   if (session.status === "authenticated" && router.pathname === "/") {
-    router.push("/dashboard");
+    pushDashboard();
   }
 
   return children;
