71e95d86575687d2e12959308a6d8bca4442df1b
akopton <kopton.olek@gmail.com>
2023-07-09 14:45:46 +0200

    layout provider route protect

--- src/components/LayoutProvider/LayoutProvider.tsx
+++ src/components/LayoutProvider/LayoutProvider.tsx
@@ -1,17 +1,21 @@
 import { useSession } from "next-auth/react";
 import { Navbar } from "../Navbar/Navbar";
+import { useRouter } from "next/router";
 
 export const LayoutProvider = ({ children }: { children: React.ReactNode }) => {
   const session = useSession();
+  const router = useRouter();
 
   if (session.status === "unauthenticated") {
     return children;
   }
 
-  return (
-    <>
-      <Navbar />
-      {children}
-    </>
-  );
+  if (session.status === "authenticated" && router.pathname !== "/") {
+    return (
+      <>
+        <Navbar />
+        {children}
+      </>
+    );
+  }
 };
