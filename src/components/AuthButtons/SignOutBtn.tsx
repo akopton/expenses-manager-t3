cfe41532ae4971e25d2fc43d8b4c50100c4f0ae9
akopton <kopton.olek@gmail.com>
2023-07-09 21:36:31 +0200

    delete unneccessary styles

--- src/components/AuthButtons/SignOutBtn.tsx
+++ src/components/AuthButtons/SignOutBtn.tsx
@@ -1,20 +1,13 @@
 import { signOut } from "next-auth/react";
-
-import { useContext } from "react";
-import { ThemeContext } from "~/context/ThemeContext";
 import styles from "./button.module.css";
 
 export const SignOutBtn = () => {
-  const { theme } = useContext(ThemeContext);
   const handleSignOut = async () => {
     await signOut();
   };
 
   return (
-    <button
-      className={`${styles.btn} ${styles[`${theme}`]}`}
-      onClick={handleSignOut}
-    >
+    <button className={styles.btn} onClick={handleSignOut}>
       Sign Out
     </button>
   );
