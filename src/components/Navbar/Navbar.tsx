bf992f91c79d2fd615f6a8f30290a3ef16ed061c
akopton <kopton.olek@gmail.com>
2023-07-10 15:05:07 +0200

    navbar styling

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -3,11 +3,14 @@ import { SignOutBtn } from "../AuthButtons/SignOutBtn";
 import { useRouter } from "next/router";
 import Link from "next/link";
 import styles from "./navbar.module.css";
+import { useState, useContext } from "react";
+import { ThemeContext } from "~/context/ThemeContext";
 
 type TLink = {
   id: number;
   name: string;
   href: string;
+  links?: TLink[];
 };
 
 export const Navbar = () => {
@@ -15,10 +18,22 @@ export const Navbar = () => {
 
   const links: TLink[] = [
     { id: 1, name: "Tablica", href: "/dashboard" },
-    { id: 2, name: "Wydatki", href: "/bills" },
+    {
+      id: 2,
+      name: "Wydatki",
+      href: "/bills",
+      links: [
+        { id: 1, name: "Kategorie", href: "/bills/categories" },
+        { id: 2, name: "Zestawy", href: "/bills/sets" },
+        { id: 3, name: "Miesiące", href: "/bills/months" },
+        { id: 4, name: "Lata", href: "/bills/years" },
+      ],
+    },
     { id: 3, name: "Analiza", href: "/analytics" },
   ];
 
+  const [showLinks, setShowLinks] = useState<boolean>(false);
+  const { theme } = useContext(ThemeContext);
   return (
     <div className={styles.container}>
       <div className={styles.themeBtn}>
@@ -31,8 +46,39 @@ export const Navbar = () => {
               pathname.includes(el.href) && styles.active
             }`}
             key={el.id}
+            onMouseEnter={() => {
+              el.links?.length && setShowLinks(true);
+            }}
+            onMouseLeave={() => {
+              el.links?.length && setShowLinks(false);
+            }}
           >
             <Link href={el.href}>{el.name}</Link>
+            {el.links && (
+              <ul
+                className={styles.additionalLinks}
+                style={{
+                  display: showLinks ? "flex" : "none",
+                  background:
+                    theme === "dark"
+                      ? "var(--primary-bg)"
+                      : "var(--primary-font)",
+                  boxShadow:
+                    theme === "dark"
+                      ? "0 8px 4px 2px var(--primary-font)"
+                      : "0 8px 4px 2px var(--primary-bg)",
+                  border: "none",
+                }}
+              >
+                {el.links.map((link) => {
+                  return (
+                    <li key={link.id} className={styles.link}>
+                      <Link href={link.href}>{link.name}</Link>
+                    </li>
+                  );
+                })}
+              </ul>
+            )}
           </li>
         ))}
       </ul>
