ba10a2ffb4196ec4876900aeb1223981e20f6eab
akopton <kopton.olek@gmail.com>
2023-07-11 13:41:44 +0200

    added router for Category model

---
+++ src/server/api/routers/categoriesRouter.ts
@@ -0,0 +1,22 @@
+import { z } from "zod";
+import { createTRPCRouter, protectedProcedure, publicProcedure } from "../trpc";
+
+export const categoriesRouter = createTRPCRouter({
+  getCategories: publicProcedure.query(async ({ ctx }) => {
+    const categories = await ctx.prisma.category.findMany();
+    return categories;
+  }),
+
+  addCategory: publicProcedure
+    .input(
+      z.object({
+        name: z.string(),
+      })
+    )
+    .mutation(async ({ input, ctx }) => {
+      const product = await ctx.prisma.category.create({
+        data: { ...input },
+      });
+      return product;
+    }),
+});
