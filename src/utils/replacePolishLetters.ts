4dc8c6c50b7e7afb082be90e25648c7d40e86078
akopton <kopton.olek@gmail.com>
2023-07-12 00:09:54 +0200

    added method to remove polish letters from string

---
+++ src/utils/replacePolishLetters.ts
@@ -0,0 +1,13 @@
+export const replacePolishLetters = (str: string) => {
+  return str
+    .toLowerCase()
+    .replace(/ą/g, "a")
+    .replace(/ć/g, "c")
+    .replace(/ę/g, "e")
+    .replace(/ł/g, "l")
+    .replace(/ń/g, "n")
+    .replace(/ó/g, "o")
+    .replace(/ś/g, "s")
+    .replace(/ź/g, "z")
+    .replace(/ż/g, "z");
+};
