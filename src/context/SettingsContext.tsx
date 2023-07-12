1e9245b3aa58479c5f5b92a067c2e17167686f15
akopton <kopton.olek@gmail.com>
2023-07-12 23:35:14 +0200

    added settings context

---
+++ src/context/SettingsContext.tsx
@@ -0,0 +1,31 @@
+import { useSession } from "next-auth/react";
+import { createContext, useState, useEffect } from "react";
+import { api } from "~/utils/api";
+
+type TSettings = {};
+
+const settings: TSettings = {};
+
+export const SettingsContext = createContext(settings);
+
+export const SettingsProvider = ({
+  children,
+}: {
+  children: React.ReactNode;
+}) => {
+  const [settings, setSettings] = useState<TSettings>();
+  const session = useSession();
+  const userId = session.data?.user.id;
+  const userSettings = { data: {} };
+  //   api.settings.getSettings.useQuery({id: userId})
+
+  useEffect(() => {
+    setSettings(userSettings.data);
+  }, [settings]);
+
+  return (
+    <SettingsContext.Provider value={{ settings }}>
+      {children}
+    </SettingsContext.Provider>
+  );
+};
