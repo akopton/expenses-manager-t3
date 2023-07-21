ba5589684aa8211fa2f042896d70d752d1851a91
akopton <kopton.olek@gmail.com>
2023-07-21 11:17:33 +0200

    rwd for loging page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -13,29 +13,23 @@ export default function Home() {
         />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className="relative flex h-screen">
+      <main className="relative flex h-screen flex-col items-center px-2 py-10 md:flex-row">
         <div className="absolute right-10 top-5">
           <ToggleThemeBtn />
         </div>
 
-        <div
-          className="flex h-full flex-grow flex-col items-center justify-center gap-10"
-          style={{
-            boxShadow: "0 0 4px 2px var(--primary-color)",
-          }}
-        >
-          <h2 className="text-3xl">Sign in now with your Google account!</h2>
+        <div className="order-2 flex h-full flex-grow flex-col items-center justify-center gap-10 px-4 md:order-1">
+          <h2 className="text-center text-3xl">
+            Sign in now with your Google account!
+          </h2>
           <SignInBtn />
         </div>
 
-        <div
-          className="flex h-full w-3/5 flex-col items-center justify-center gap-10"
-          style={{
-            boxShadow: "0 0 4px 2px var(--primary-color)",
-          }}
-        >
-          <h1 className="text-5xl">Expenses Tracker App</h1>
-          <p className="text-center text-2xl">
+        <div className="order-1 flex h-full w-full flex-col items-center justify-center gap-10 px-4 md:order-2 md:w-3/5 md:border-l-4 md:border-primaryColor">
+          <h1 className="text-center text-3xl md:text-5xl">
+            Expenses Tracker App
+          </h1>
+          <p className="text-center text-xl md:text-2xl">
             Best solution to store your bills,
             <br /> track and analyze your expenses!
           </p>
